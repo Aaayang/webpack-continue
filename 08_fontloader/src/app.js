6b3f1a8412db2c5c90a9434f806c4fa18c5f525b
Aaayang <iamajser@163.com>
2017-12-03 17:57:33 +0800

    08_使用第三方字体

--- 08_fontloader/src/app.js
+++ 08_fontloader/src/app.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./common/style/main.css";
-
+// import "./common/style/main.css";
+import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(
-    <div className="rocket">hello world</div>,
+    <div className="fa fa-rocket">hello world</div>,
     document.getElementById("root")
 );
